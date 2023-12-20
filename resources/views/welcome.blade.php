c04d48311274f304e7fff59729dc7d5a7b169953
victor braz <victor.90braz@gmail.com>
2023-12-20 17:16:54 +0100

    define dashboard layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,60 +32,10 @@
                 <div class="mx-auto max-w-2xl px-4 py-16 sm:py-8 lg:max-w-7xl lg:px-8">
                     <div class="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
                         <a href="#" class="group">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                        </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-                        <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-                        </a>
-                        <a href="#" class="group">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                        </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-                        <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-                        </a>
-                        <a href="#" class="group">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                        </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-                        <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-                        </a>
-                        <a href="#" class="group">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                        </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-                        <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-                        </a>
-                        <a href="#" class="group">
-                            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                                <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                            </div>
-                            <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-                            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-                        </a>
-                        <a href="#" class="group">
-                            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                                <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                            </div>
-                            <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-                            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-                        </a>
-                        <a href="#" class="group">
-                            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                                <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                            </div>
-                            <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-                            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-                        </a>
-                        <a href="#" class="group">
-                            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                                <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-                            </div>
-                            <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-                            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
+                            <figure class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-black xl:aspect-h-8 xl:aspect-w-7">
+                                <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
+                                <figcaption class="p-2 text-sm text-white flex flex-col items-center justify-center">Earthen Bottle</figcaption>
+                            </figure>
                         </a>
                     </div>
                 </div>
